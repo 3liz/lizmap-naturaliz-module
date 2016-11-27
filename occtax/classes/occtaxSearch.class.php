7fb7765b73a69c6840ac1f53c9edc5fd5effa31d
Michael Douchin <mdouchin@3liz.com>
2016-11-27 14:27:53 +0100

    Occtax - Improve demand system with validity dates

--- occtax/classes/occtaxSearch.class.php
+++ occtax/classes/occtaxSearch.class.php
@@ -293,7 +293,7 @@ class occtaxSearch {
         $this->sql.= $this->whereClause;
         $this->sql.= $this->groupClause;
 
-jLog::log($this->sql);
+//jLog::log($this->sql);
     }
 
 
@@ -450,6 +450,7 @@ jLog::log($this->sql);
     private function getWhereClauseDemande(){
         $return = '';
         $table_demandes = array();
+        $cnx = jDb::getConnection();
 
         // Get user info
         $user = jAuth::getUserSession();
@@ -466,7 +467,9 @@ jLog::log($this->sql);
             "geom" => array()
         );
         foreach($demandes as $demande){
+            $sql_demande = array();
 
+            // First build occtax search with demande params
             $dparams = array();
             if($demande->cd_ref)
                 $dparams['cd_nom'] = explode( ',', trim($demande->cd_ref, '{}') );
@@ -474,37 +477,39 @@ jLog::log($this->sql);
                 $dparams['group1_inpn'] = explode( ',', trim($demande->group1_inpn, '{}') );
             if($demande->group2_inpn)
                 $dparams['group2_inpn'] = explode( ',', trim($demande->group2_inpn, '{}') );
-            if($demande->date_validite_min)
-                $dparams['date_min'] = $demande->date_validite_min;
-            if($demande->date_validite_max)
-                $dparams['date_max'] = $demande->date_validite_max;
 
             $dsearch = new occtaxSearchObservation( null, $dparams, 1 );
-            $wc = preg_replace(
+            $sql_demande[] = preg_replace(
                 '/WHERE +True( +AND +)?/i',
                 '',
                 $dsearch->getWhereClause()
             );
 
+            // Add geometry filter if set
             if($demande->geom){
                 $cnx = jDb::getConnection();
-                $wc_geom = ' ST_Intersects(o.geom, ST_GeomFromText(' . $cnx->quote($demande->geom) . ', '. $this->srid .')) ' ;
-                if(!(empty(trim($wc)))){
-                    $wc.= " AND " . $wc_geom;
-                }else{
-                    $wc.= $wc_geom;
-                }
+                $sql_geom = ' ST_Intersects(o.geom, ST_GeomFromText(' . $cnx->quote($demande->geom) . ', '. $this->srid .')) ' ;
+                $sql_demande[] = $sql_geom;
             }
 
-            if(!empty(trim($wc))){
-                $table_demandes[] = ' ( ' . $wc . ' ) ';
+            // Add validity dates
+            if($demande->date_validite_min){
+                $sql_demande[] = ' ' . $cnx->quote($demande->date_validite_min) . '::date <= now()::date' ;
+            }
+            if($demande->date_validite_max){
+                $sql_demande[] = ' now()::date <= ' . $cnx->quote($demande->date_validite_max) . '::date ' ;
+            }
+
+            // Build full sql for this demand
+            if(count($sql_demande) > 0){
+                $table_demandes[] = ' ( ' . implode( ' AND ', $sql_demande) . ' ) ';
             }
 
         }
         if( count($table_demandes)>0 ){
             $return = implode( ' OR ', $table_demandes);
             $return = ' AND ( ' . $return . ' ) ';
-
+//jLog::log($return);
         }
         return $return;
     }
