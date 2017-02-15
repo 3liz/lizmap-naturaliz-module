5447ff3ac79d5b3c166e477109eabc3022952096
Michael Douchin <mdouchin@3liz.com>
2017-02-15 17:42:48 +0100

    Gestion - Remove NOT NULL on demande.usr_login and link acteur & demande

--- gestion/install/sql/install.pgsql.sql
+++ gestion/install/sql/install.pgsql.sql
@@ -26,7 +26,8 @@ CREATE INDEX ON g_nomenclature (champ, code);
 -- Table demande
 CREATE TABLE demande (
     id serial PRIMARY KEY,
-    usr_login character varying(50) NOT NULL,
+    usr_login character varying(50),
+    id_acteur integer NOT NULL,
     id_organisme integer NOT NULL,
     motif text NOT NULL,
     type_demande text NOT NULL,
@@ -46,8 +47,13 @@ ALTER TABLE demande ALTER COLUMN geom SET NOT NULL;
 ALTER TABLE demande ADD CONSTRAINT demande_user_login_fk
 FOREIGN KEY (usr_login) REFERENCES jlx_user (usr_login)
 ON DELETE RESTRICT;
+
 ALTER TABLE demande ADD CONSTRAINT demande_id_organisme_fk
 FOREIGN KEY (id_organisme) REFERENCES occtax."organisme" (id_organisme)
+ON DELETE RESTRICT;
+
+ALTER TABLE demande ADD CONSTRAINT demande_id_acteur_fk
+FOREIGN KEY (id_acteur) REFERENCES occtax."acteur" (id_acteur)
 ON DELETE RESTRICT;
 
 ALTER TABLE demande ADD CONSTRAINT demande_valide
@@ -56,8 +62,9 @@ CHECK ( Coalesce(cd_ref::text, group1_inpn::text, group2_inpn::text, '') != '' )
 
 COMMENT ON TABLE demande IS 'Liste des demandes d''acccès à l''application. Cette table permet de restreindre les accès aux données, par date, taxon, etc.';
 COMMENT ON COLUMN demande.id IS 'Identifiant auto de la demande (clé primaire)';
-COMMENT ON COLUMN demande.usr_login IS 'Login de l''utilisateur qui fait la demande, pour lequel activer une restriction. On peut avoir plusieurs lignes pour la demande. Clé étrangère vers publi.jlx_user';
+COMMENT ON COLUMN demande.usr_login IS 'Login de l''utilisateur qui fait la demande, pour lequel activer une restriction. On peut avoir plusieurs lignes qui référencent le usr_login. Ce champ peut être vide quand on a pas encore validé la demande (et si on ne la valide jamais). Clé étrangère vers publi.jlx_user';
 COMMENT ON COLUMN demande.id_organisme IS 'Identifiant de l''organisme ayant émis la demande. Clé étrangère vers table organisme';
+COMMENT ON COLUMN demande.id_acteur IS 'Identifiant de l''acteur ayant émis la demande. Clé étrangère vers table acteur';
 COMMENT ON COLUMN demande.motif IS 'Motif de la demande d''accès aux données fourni par le demandeur';
 COMMENT ON COLUMN demande.type_demande IS 'Type de demande selon la typologie de la charte du SINP (exemple : mission régalienne, publication scientifique, etc.)';
 COMMENT ON COLUMN demande.date_demande IS 'Date d''émission de la demande (découplée de la date de création, qui est elle renseignée automatiquement';
@@ -84,7 +91,6 @@ CREATE TABLE acteur(
     tel_2 text,
     fonction text,
     id_organisme integer NOT NULL,
-    usr_login text,
     remarque text,
     bulletin_information boolean default FALSE,
     reunion_sinp boolean default FALSE
@@ -96,12 +102,6 @@ ON UPDATE RESTRICT
 ON DELETE RESTRICT
 ;
 
-ALTER TABLE acteur ADD CONSTRAINT acteur_usr_login_fkey
-FOREIGN KEY (usr_login)
-REFERENCES public.jlx_user(usr_login) MATCH SIMPLE
-ON UPDATE RESTRICT
-ON DELETE RESTRICT
-;
 
 COMMENT ON TABLE acteur IS 'Liste les acteurs liés à l''application. Cette table sert à stocker les personnes ressource: responsables des imports de données, référents des jeux de données, etc.';
 COMMENT ON COLUMN acteur.id_acteur IS 'Identifiant de l''acteur (entier auto-incrémenté)';
@@ -113,7 +113,6 @@ COMMENT ON COLUMN acteur.tel_1 IS 'Numéro de téléphone principal de l''acteur
 COMMENT ON COLUMN acteur.tel_2 IS 'Numéro de téléphone secondaire de l''acteur';
 COMMENT ON COLUMN acteur.fonction IS 'Fonction de l''acteur (champ libre)';
 COMMENT ON COLUMN acteur.id_organisme IS 'Identifiant de l''organisme de l''acteur (clé étrangère vers table organisme)';
-COMMENT ON COLUMN acteur.usr_login IS 'Login de l''acteur. Ce login doit correspondre au contenu de la table public.jlx_user';
 COMMENT ON COLUMN acteur.remarque IS 'Remarque sur l''acteur (texte libre)';
 COMMENT ON COLUMN acteur.bulletin_information IS 'Indique si l''acteur souhaite recevoir le bulletin d''information par courriel.';
 COMMENT ON COLUMN acteur.reunion_sinp IS 'Indique si l''acteur participe aux réunion du SINP local.';
@@ -144,4 +143,5 @@ ON DELETE RESTRICT
 
 -- INDEXES
 CREATE INDEX ON demande (usr_login);
-CREATE INDEX ON acteur (usr_login);
+CREATE INDEX ON demande (id_acteur);
+
