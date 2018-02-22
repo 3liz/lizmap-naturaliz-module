441998c1afd49f6922dbc6db838b264b9dc47fe9
Michael Douchin <mdouchin@3liz.com>
2018-02-22 15:47:08 +0100

    Taxon - Support de Taxref V11 : ajout fichier SQL pour v11

---
+++ taxon/install/sql/import.taxref.11.pgsql.sql
@@ -0,0 +1,8 @@
+TRUNCATE taxref RESTART IDENTITY CASCADE;
+COPY taxref
+(regne, phylum, classe, ordre, famille, sous_famille, tribu, group1_inpn, group2_inpn,
+ cd_nom, cd_taxsup, cd_sup, cd_ref, rang, lb_nom, lb_auteur, nom_complet,
+ nom_complet_html, nom_valide, nom_vern, nom_vern_eng, habitat,
+ fr, gf, mar, gua, sm, sb, spm, may, epa, reu, sa, ta, taaf, pf, nc, wf, cli, url)
+FROM '{$source}' DELIMITER E'\t' HEADER CSV;
+RESET client_encoding;
