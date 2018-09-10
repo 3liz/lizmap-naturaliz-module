2c32977236ec299838375e9beb223c22a26333cf
Michael Douchin <mdouchin@3liz.com>
2018-09-10 11:39:26 +0200

    Taxon - Correction d'un souci dans la dernière migration SQL

---
+++ taxon/install/upgrade_220_221.php
@@ -0,0 +1,31 @@
+<?php
+class taxonModuleUpgrader_220_221 extends jInstallerModule {
+
+    public $targetVersions = array(
+        '2.2.1'
+    );
+    public $date = '2018-09-10';
+
+    function install() {
+        if( $this->firstDbExec() ) {
+            // modify jlx_user columns
+            $this->useDbProfile('jauth_super');
+            $sqlPath = $this->path . 'install/sql/upgrade/upgrade_2.2.0_2.2.1.sql';
+            $sql = jFile::read( $sqlPath );
+            $db = $this->dbConnection();
+            $db->exec($sql);
+
+            // Grant rights
+            $sqlPath = $this->path . 'install/sql/grant_rights.sql';
+            $sqlTpl = jFile::read( $sqlPath );
+            $tpl = new jTpl();
+            $prof = jProfiles::get('jdb', $this->dbProfile, true);
+            $tpl->assign('DBNAME', $prof['database'] );
+            $tpl->assign('DBUSER_READONLY', 'naturaliz' );
+            $tpl->assign('DBUSER_OWNER', 'lizmap' );
+            $sql = $tpl->fetchFromString($sqlTpl, 'text');
+            $db = $this->dbConnection();
+            $db->exec($sql);
+        }
+    }
+}
