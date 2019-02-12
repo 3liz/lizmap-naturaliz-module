fa18f0fce27b4f91bb352ab9dd75a32557947c31
Michael Douchin <mdouchin@3liz.com>
2019-02-12 16:07:18 +0100

    General - Fix bug sur lancement GRANT RIGHT si nom d'utilisateur complexe

--- occtax/install/sql/grant_rights.sql
+++ occtax/install/sql/grant_rights.sql
@@ -1,21 +1,21 @@
 BEGIN;
 
 -- Ajout des droits sur les objets de la base pour naturaliz
-GRANT CONNECT ON DATABASE {$DBNAME} TO {$DBUSER_READONLY};
-GRANT USAGE ON SCHEMA public,taxon,sig,occtax,gestion TO {$DBUSER_READONLY};
-GRANT SELECT ON ALL TABLES IN SCHEMA occtax,sig,taxon,gestion TO {$DBUSER_READONLY};
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO {$DBUSER_READONLY};
-GRANT INSERT ON ALL TABLES IN SCHEMA occtax TO {$DBUSER_READONLY};
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public,occtax,sig,taxon,gestion TO {$DBUSER_READONLY};
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public,occtax,sig,taxon,gestion TO {$DBUSER_READONLY};
-ALTER ROLE {$DBUSER_READONLY} SET search_path TO taxon,occtax,gestion,sig,public;
+GRANT CONNECT ON DATABASE {$DBNAME} TO "{$DBUSER_READONLY}";
+GRANT USAGE ON SCHEMA public,taxon,sig,occtax,gestion TO "{$DBUSER_READONLY}";
+GRANT SELECT ON ALL TABLES IN SCHEMA occtax,sig,taxon,gestion TO "{$DBUSER_READONLY}";
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "{$DBUSER_READONLY}";
+GRANT INSERT ON ALL TABLES IN SCHEMA occtax TO "{$DBUSER_READONLY}";
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public,occtax,sig,taxon,gestion TO "{$DBUSER_READONLY}";
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public,occtax,sig,taxon,gestion TO "{$DBUSER_READONLY}";
+ALTER ROLE "{$DBUSER_READONLY}" SET search_path TO taxon,occtax,gestion,sig,public;
 
 -- Ajout des droits sur les objets de la base pour lizmap
-GRANT CONNECT ON DATABASE {$DBNAME} TO {$DBUSER_OWNER};
-GRANT ALL PRIVILEGES ON SCHEMA public,taxon,sig,occtax,gestion TO {$DBUSER_OWNER};
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public,taxon,sig,occtax,gestion TO {$DBUSER_OWNER};
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public,occtax,sig,taxon,gestion TO {$DBUSER_OWNER};
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public,occtax,sig,taxon,gestion TO {$DBUSER_OWNER};
-ALTER ROLE {$DBUSER_OWNER} SET search_path TO taxon,occtax,gestion,sig,public;
+GRANT CONNECT ON DATABASE {$DBNAME} TO "{$DBUSER_OWNER}";
+GRANT ALL PRIVILEGES ON SCHEMA public,taxon,sig,occtax,gestion TO "{$DBUSER_OWNER}";
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public,taxon,sig,occtax,gestion TO "{$DBUSER_OWNER}";
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public,occtax,sig,taxon,gestion TO "{$DBUSER_OWNER}";
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public,occtax,sig,taxon,gestion TO "{$DBUSER_OWNER}";
+ALTER ROLE "{$DBUSER_OWNER}" SET search_path TO taxon,occtax,gestion,sig,public;
 
 COMMIT;
