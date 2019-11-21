6a73d0e50cb790c1046d5da5ec85337847bdcf21
Michael Douchin <mdouchin@3liz.com>
2019-11-21 18:32:21 +0100

    Fix bug with GRANT install script

--- occtax/install/sql/grant_rights.sql
+++ occtax/install/sql/grant_rights.sql
@@ -8,7 +8,6 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "{$DBUSER_READONLY}";
 GRANT INSERT ON ALL TABLES IN SCHEMA occtax TO "{$DBUSER_READONLY}";
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public,occtax,sig,taxon,gestion TO "{$DBUSER_READONLY}";
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public,occtax,sig,taxon,gestion TO "{$DBUSER_READONLY}";
-ALTER ROLE "{$DBUSER_READONLY}" SET search_path TO taxon,occtax,gestion,sig,public;
 
 -- Ajout des droits sur les objets de la base pour lizmap
 GRANT CONNECT ON DATABASE {$DBNAME} TO "{$DBUSER_OWNER}";
@@ -16,6 +15,5 @@ GRANT ALL PRIVILEGES ON SCHEMA public,taxon,sig,occtax,gestion TO "{$DBUSER_OWNE
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public,taxon,sig,occtax,gestion TO "{$DBUSER_OWNER}";
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public,occtax,sig,taxon,gestion TO "{$DBUSER_OWNER}";
 GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public,occtax,sig,taxon,gestion TO "{$DBUSER_OWNER}";
-ALTER ROLE "{$DBUSER_OWNER}" SET search_path TO taxon,occtax,gestion,sig,public;
 
 COMMIT;
