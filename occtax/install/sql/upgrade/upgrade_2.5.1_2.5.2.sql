1688dd7a28e37d1a1f436dcc6af7ba2d5ac6d2a6
Michael Douchin <mdouchin@3liz.com>
2020-03-30 18:27:25 +0200

    BDD - Correction contrainte obs_objet_denombrement_valide sur occtax.observation

---
+++ occtax/install/sql/upgrade/upgrade_2.5.1_2.5.2.sql
@@ -0,0 +1,7 @@
+
+ALTER TABLE occtax.observation DROP CONSTRAINT IF EXISTS obs_objet_denombrement_valide;
+ALTER TABLE occtax.observation ADD CONSTRAINT obs_objet_denombrement_valide CHECK (
+    ( denombrement_min IS NOT NULL AND denombrement_max IS NOT NULL AND objet_denombrement IN ('COL', 'CPL', 'HAM', 'IND', 'NID', 'NSP', 'PON', 'SURF', 'TIGE', 'TOUF')  )
+    OR (denombrement_min IS NULL AND denombrement_max IS NULL AND Coalesce(objet_denombrement, 'NSP') = 'NSP')
+);
+
